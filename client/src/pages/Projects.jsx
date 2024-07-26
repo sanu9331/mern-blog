ce58bd61ec91d9fad228c959febaa7a6aff5c573
sanu <sanusk9331@gmail.com>
2024-07-26 12:20:10 +0530

    added build script in package.json

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Projects() {
     return (
         <div>
-            <h1>Projects</h1>
+            <h1> hi ,Projects</h1>
+            <h3> you can add user projects here</h3>
         </div>
     )
 }
